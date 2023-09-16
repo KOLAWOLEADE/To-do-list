d756e9c7c73036ac47f75ab4b61387c95a87b8ed
Dadeesamuel <dadeesamuel@gmail.com>
2023-09-16 21:04:20 +0100

    styling for to-do list

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+
+function addTask() {
+    const taskText = document.getElementById("new-task").value;
+    if (taskText.trim() === "") {
+        return;
+    }
+    const taskList = document.getElementById("todo-list");
+    const taskItem = document.createElement("li");
+    taskItem.innerHTML = `
+        <span>${taskText}</span>
+        <button onclick="removeTask(this)">Delete</button>
+    `;
+    taskList.appendChild(taskItem);
+    document.getElementById("new-task").value = "";
+}
+
+function removeTask(button) {
+    const taskItem = button.parentElement;
+    const taskList = document.getElementById("todo-list");
+    taskList.removeChild(taskItem);
+}
+
+function clearTasks() {
+    const taskList = document.getElementById("todo-list");
+    taskList.innerHTML = "";
+}
