32b688edc4d06bb8c44bc0533be2b58307e6a599
Olamilekan <olamilekanomosanya@gmail.com>
2023-09-19 22:13:47 +0100

    add footer

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,9 @@
 
-function addTask() {
-    const taskText = document.getElementById("new-task").value;
-    if (taskText.trim() === "") {
-        return;
-    }
-    const taskList = document.getElementById("todo-list");
-    const taskItem = document.createElement("li");
-    taskItem.innerHTML = `
-        <span>${taskText}</span>
-        <button onclick="removeTask(this)">Delete</button>
-    `;
-    taskList.appendChild(taskItem);
-    document.getElementById("new-task").value = "";
-}
 
-function removeTask(button) {
-    const taskItem = button.parentElement;
-    const taskList = document.getElementById("todo-list");
-    taskList.removeChild(taskItem);
-}
 
-function clearTasks() {
-    const taskList = document.getElementById("todo-list");
-    taskList.innerHTML = "";
+
+function TodoList(){
+    //pseudocode
+    this.task = task;
+    this.name = name;
 }
