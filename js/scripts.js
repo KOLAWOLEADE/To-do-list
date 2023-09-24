ef03b7a90c0bfd43322ffb5bb7f897bdfee9dbd9
kolawole adeyemo <kolawole_adeyemo@yahoo.com>
2023-09-24 19:52:01 +0100

    add dalytasks to the group project

--- js/scripts.js
+++ js/scripts.js
@@ -31,19 +31,50 @@ function clearTasks() {
 
 
 
-function toDoList {
+function toDoList() {
 
-    this.Dailytasks = {}
+    this.dailytasks = {};
     // this.Dates = {}
     // this.times = {}
-    this.currentid = 0
+    this.currentid = 0;
+}
+
+toDoList.prototype.addDaily = function(daily){
+
+    daily.id = this.assignid();
+    this.dailytasks[daily.id] = daily;
+
+}
+
+toDoList.prototype.assignid = function(){
+       this.currentid ++;
+       return this.currentid
 }
 
 
-function Dailytask {
-    this.dailytask = dailytask
-    this.date = date
-    this.time = time
+AddressBook.prototype.findDaily = function(id) {
+    if (this.dailytasks[id] != undefined) {
+      return this.dailytasks[id];
+    }
+    return false;
+  };
+  
+  AddressBook.prototype.deleteDaily = function(id) {
+    if (this.dailytasks[id] === undefined) {
+      return false;
+    }
+    delete this.dailytasks[id];
+    return true;
+  };
+
+function Dailytask(dailytask, date, time) {
+    this.dailytask = dailytask;
+    this.date = date;
+    this.time = time;
 
 
+}
+
+Dailytask.prototype.taskDetail = function() {
+    return this.dailytask + " " + this.date + " " + this.time
 }
